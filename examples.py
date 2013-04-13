d9bb4736adc99486ccab51ac42355c15682263f5
Joshua Carp <jm.carp@gmail.com>
2013-04-13 14:41:21 -0400

    added apa scraper

--- examples.py
+++ examples.py
@@ -2,6 +2,17 @@
 import sciscrape
 import searchscrape
 
+def example_scrape_jep():
+    '''Search PubMed for fMRI studies published
+    in Neuroimage, download article documents,
+    and save results to current working directory.'''
+    
+    searchscrape.searchscrape(
+        'fmri AND "journal of experimental psychology. human perception and performance"[journal]',
+        '.',
+        scrape_klass = sciscrape.UMSciScrape,
+        retmax=5
+    )
 def example_scrape_nimg():
     '''Search PubMed for fMRI studies published
     in Neuroimage, download article documents,
