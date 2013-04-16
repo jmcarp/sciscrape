05d8e76450555f4d2fe82610f40b19508c44b126
Joshua Carp <jm.carp@gmail.com>
2013-04-16 14:02:31 -0400

    removed metaclasses, switched to class registry

--- pubdet.py
+++ pubdet.py
@@ -7,175 +7,115 @@ def pubdet(html):
     '''Detect publisher from HTML document.'''
 
     publisher = None
-    for detklass in PubDetector.__inheritors__:
-        detobj = detklass()
-        if detobj.detect(html):
-            return detobj._name
+    for detector in PubDetector.registry:
+        if PubDetector.registry[detector].detect(html):
+            return detector
 
 class PubDetector(object):
-    '''Base class for publisher detectors.'''
+    '''Detect publisher based on arbitrary function.'''
+    
+    # Initialize detector registry
+    registry = {}
 
-    class __metaclass__(type):
-        '''Custom metaclass to track inheritors.'''
-
-        __inheritors__ = []
+    def __init__(self, name, fun):
+        '''Bind function to detect() method, then add self
+        to registry.'''
         
-        def __new__(meta, name, bases, dct):
-            '''When creating a subclass, add to __inheritors__ if 
-            _name class property is defined.'''
-            klass = type.__new__(meta, name, bases, dct)
-            try:
-                _name = klass()._name
-                meta.__inheritors__.append(klass)
-            except NotImplementedError:
-                pass
-            return klass 
-
-    @property
-    def _name(self):
-        raise NotImplementedError('Subclasses must define _name')
-
-    def detect(self, html):
-        raise NotImplementedError('Subclasses must implement detect()')
-
-######################################
-# PubDetectors based on <title> tags #
-######################################
+        self.detect = fun
+        PubDetector.registry[name] = self
 
 class TitlePubDetector(PubDetector):
-    '''Base class for <title> tag detectors.'''
+    '''Detect publisher based on <title> tag.'''
     
-    _flags = re.I
-
-    def detect(self, html):
-
-        title = PyQuery(html)('title')
-        if title:
-            return bool(re.search(self._regex, title.text(), self._flags))
-        return False
-    
-class ElsevierDetector(TitlePubDetector):
-    
-    _name = 'elsevier'
-    _regex = 'sciencedirect'
-
-class Springer(TitlePubDetector):
-    
-    _name = 'springer'
-    _regex = 'springer'
-
-class WoltersKluwer(TitlePubDetector):
-    
-    _name = 'wolterskluwer'
-    _regex = 'wolters kluwer'
-
-#####################################
-# PubDetectors based on <meta> tags #
-#####################################
+    def __init__(self, name, regex, flags=re.I):
+        
+        # Build detector function
+        def fun(html):
+            title = PyQuery(html)('title')
+            if title:
+                return bool(re.search(regex, title.text(), flags))
+            return False
+        
+        # Call super constructor
+        super(TitlePubDetector, self).__init__(name, fun)
 
 class MetaPubDetector(PubDetector):
-    '''Base class for <meta> tag detectors.'''
+    '''Detect publisher based on <meta> tags.'''
     
-    _ops = '='
+    def __init__(self, name, attrs, opers='='):
+        
+        # Build detector function
+        def fun(html):
+            return bool(PyQuery(html)(utils.build_query(
+                'meta', attrs, opers
+            )))
+        
+        # Call super constructor
+        super(MetaPubDetector, self).__init__(name, fun)
 
-    def detect(self, html):
-        return bool(PyQuery(html)(utils.build_query(
-            'meta', self._attrs, self._ops
-        )))
+# Define detectors based on <title> tag
+TitlePubDetector('elsevier', 'sciencedirect')
+TitlePubDetector('springer', 'springer')
+TitlePubDetector('wolterskluwer', 'wolters kluwer')
 
-class HighwireDetector(MetaPubDetector):
-    
-    _name = 'highwire'
-    _attrs = [
+# Define detectors based on <meta> tags
+MetaPubDetector('highwire', [
         ['name', 'HW.identifier'],
     ]
-
-class TaylorFrancisDetector(MetaPubDetector):
-    
-    _name = 'tandf'
-    _attrs = [
-        ['property', 'og:site_name'],
-        ['content', 'Taylor and Francis'],
-    ]
-
-class ThiemeDetector(MetaPubDetector):
-    
-    _name = 'thieme'
-    _attrs = [
-        ['name', 'citation_publisher'],
-        ['content', 'Thieme Medical Publishers'],
-    ]
-
-class APA(MetaPubDetector):
-    
-    _name = 'apa'
-    _attrs = [
+)
+MetaPubDetector('apa', [
         ['name', 'citation_publisher'],
         ['content', 'American Psychological Association'],
     ]
-
-class BMCDetector(MetaPubDetector):
-    
-    _name = 'bmc'
-    _attrs = [
+)
+MetaPubDetector('tandf', [
+        ['property', 'og:site_name'],
+        ['content', 'Taylor and Francis'],
+    ]
+)
+MetaPubDetector('thieme', [
         ['name', 'citation_publisher'],
-        ['content', 'BioMed Central Ltd'],
+        ['content', 'Thieme Medical Publishers'],
     ]
-
-class NASDetector(MetaPubDetector):
-    
-    _name = 'nas'
-    _attrs = [
-        ['name', 'citation_publisher'],
-        ['content', 'National Acad Sciences'],
-    ]
-
-class PLOSDetector(MetaPubDetector):
-    
-    _name = 'plos'
-    _attrs = [
-        ['name', 'citation_publisher'],
-        ['content', 'Public Library of Science'],
-    ]
-
-class FrontiersDetector(MetaPubDetector):
-    
-    _name = 'frontiers'
-    _attrs = [
-        ['name', 'citation_publisher'],
-        ['content', 'Frontiers'],
-    ]
-
-class AMADetector(MetaPubDetector):
-    
-    _name = 'ama'
-    _attrs = [
-        ['name', 'citation_publisher'],
-        ['content', 'American Medical Association'],
-    ]
-
-class NatureDetector(MetaPubDetector):
-    
-    _name = 'npg'
-    _attrs = [
-        ['name', 'DC.publisher'],
-        ['content', 'Nature Publishing Group'],
-    ]
-
-class MITDetector(MetaPubDetector):
-    
-    _name = 'mit'
-    _attrs = [
-        ['name', 'dc.Publisher'],
-        ['content', 'MIT Press'],
-    ]
-    _ops = ['=', '^=']
-
-class RoyalSocietyDetector(MetaPubDetector):
-    
-    _name = 'royal'
-    _attrs = [
+)
+MetaPubDetector('royal', [
         ['name', 'DC.Publisher'],
         ['content', 'The Royal Society'],
     ]
-
+)
+MetaPubDetector('mit', [
+        ['name', 'dc.Publisher'],
+        ['content', 'MIT Press'],
+    ],
+    ['=', '^='],
+)
+MetaPubDetector('npg', [
+        ['name', 'DC.publisher'],
+        ['content', 'Nature Publishing Group'],
+    ]
+)
+MetaPubDetector('ama', [
+        ['name', 'citation_publisher'],
+        ['content', 'American Medical Association'],
+    ]
+)
+MetaPubDetector('plos', [
+        ['name', 'citation_publisher'],
+        ['content', 'Public Library of Science'],
+    ]
+)
+MetaPubDetector('frontiers', [
+        ['name', 'citation_publisher'],
+        ['content', 'Frontiers'],
+    ]
+)
+MetaPubDetector('nas', [
+        ['name', 'citation_publisher'],
+        ['content', 'National Acad Sciences'],
+    ]
+)
+MetaPubDetector('bmc', [
+        ['name', 'citation_publisher'],
+        ['content', 'BioMed Central Ltd'],
+    ]
+)
