5f441f08d52709737a8b8badfbc28c620e66e66d
Joshua Carp <jm.carp@gmail.com>
2013-04-18 19:36:29 -0400

    added neuroreport example

--- examples.py
+++ examples.py
@@ -13,6 +13,7 @@ def example_scrape_jep():
         scrape_klass = sciscrape.UMSciScrape,
         retmax=5
     )
+
 def example_scrape_nimg():
     '''Search PubMed for fMRI studies published
     in Neuroimage, download article documents,
@@ -20,6 +21,18 @@ def example_scrape_nimg():
     
     searchscrape.searchscrape(
         'fmri AND neuroimage[journal]',
+        '.',
+        scrape_klass = sciscrape.UMSciScrape,
+        retmax=5
+    )
+
+def example_scrape_nrep():
+    '''Search PubMed for fMRI studies published
+    in Neuroimage, download article documents,
+    and save results to current working directory.'''
+    
+    searchscrape.searchscrape(
+        'fmri AND neuroreport[journal]',
         '.',
         scrape_klass = sciscrape.UMSciScrape,
         retmax=5
