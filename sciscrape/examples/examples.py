5e66aa71c2c921202c6142e903cfda0b374d6f36
Joshua Carp <jm.carp@gmail.com>
2013-04-20 14:45:07 -0400

    added comments

--- sciscrape/examples/examples.py
+++ sciscrape/examples/examples.py
@@ -1,6 +1,42 @@
+'''
+Miscellaneous examples of sciscrape usage.
+'''
+
 # Project imports
 from sciscrape.scrapetools import scrape
 from sciscrape.scrapetools import searchscrape
+from sciscrape.parsetools import sciparse
+
+def example_full_stream(doi='10.1162/jocn.2009.21274', 
+                        scrape_klass=scrape.Scrape,
+                        out_dir=None):
+    '''Process an article, from start (DOI) to finish (parsed tables).
+
+    Args:
+        doi : Article DOI
+        scrape_klass : (Sub-) class of scrape.Scrape
+        out_dir : Directory to save scraped documents
+
+    '''
+    
+    # Instantiate Scrape (sub-) class
+    scraper = scrape_klass()
+    
+    # Call scrape() on article DOI
+    scrape_info = scraper.scrape(doi=doi)
+
+    # Save documents to output directory
+    if out_dir:
+        scrape_info.save(out_dir)
+    
+    if 'pdf' in scrape_info.docs:
+
+        # Parse PDF
+        parser = sciparse.PDFTableParse()
+        tables = parser.parse(scrape_info.docs['pdf'])
+
+        # Return parsed tables
+        return tables
 
 def example_scrape_jep():
     '''Search PubMed for fMRI studies published
