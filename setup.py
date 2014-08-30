578c69e10b35b2a04264a7cda7f7c4da405ab9ec
jmcarp <jm.carp@gmail.com>
2014-08-30 14:37:33 -0400

    Include requirements during installation.

--- setup.py
+++ setup.py
@@ -1,10 +1,17 @@
+# -*- coding: utf-8 -*-
+
 import os
 from setuptools import setup, find_packages
+from pip.req import parse_requirements
 
 setup(
     name='sciscrape',
     version='0.1',
     packages=find_packages(),
     package_data={'' : ['tests/data/csv/*/*.csv']},
-    include_package_data=True
+    include_package_data=True,
+    install_requires=[
+        str(requirement.req)
+        for requirement in parse_requirements('requirements.txt')
+    ],
 )
